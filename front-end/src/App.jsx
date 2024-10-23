9a026295545f8925e49cb7b0e49f98dfab82622a
xpresso <periodic.table1947@gmail.com>
2024-10-24 02:50:08 +0530

    chore: initial react code setup

---
+++ front-end/src/App.jsx
@@ -0,0 +1,19 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import './App.css';
+import Navbar from './components/Navbar';
+import { Home } from './pages/Home';
+
+function App() {
+	return (
+		<>
+			<BrowserRouter>
+				<Navbar />
+				<Routes>
+					<Route path='/' element={<Home />} />
+				</Routes>
+			</BrowserRouter>
+		</>
+	);
+}
+
+export default App;
---
+++ front-end/src/pages/Home.jsx
@@ -0,0 +1,3 @@
+export const Home = () => {
+	return <div className="bg-rose-500 text-2xl p-3">Home Page</div>;
+};
